0ffc4b00b07f5763851667d241ec161920f8c0d9
renfliu <i@renf.me>
2018-11-17 21:44:21 +0800

    修改find，可以从某一个特定房间开始搜索

--- ucas.py
+++ ucas.py
@@ -20,33 +20,34 @@ def login(username, password, prefix=False):
     respHost,respSearch = respUrl.split('?')
     if 'success' in respHost:
         print("Network is connected!")
-        exit()
-    else:
-        if prefix:
-            username = "%E5%8F%91%5C" + username
-            print("user:" + username + "  pass:" + password)
-        params = {
-            'userId': username,
-            'password': password,
-            'service': '',
-            'queryString': respSearch,
-            'operatorPwd': '',
-            'operatorUserId': '',
-            'validcode': '',
-            'passwordEncrypt': 'false'
-        }
-        data = urlencode(params).encode('utf-8')
-        req = Request('http://210.77.16.21/eportal/InterFace.do?method=login', data=data)
-        loginResp = urlopen(req)
-        loginResult = json.loads(loginResp.read().decode('utf-8'))
+        return False
+    
+    if prefix:
+        username = "%E5%8F%91%5C" + username
+        print("user:" + username + "  pass:" + password)
+    params = {
+        'userId': username,
+        'password': password,
+        'service': '',
+        'queryString': respSearch,
+        'operatorPwd': '',
+        'operatorUserId': '',
+        'validcode': '',
+        'passwordEncrypt': 'false'
+    }
+    data = urlencode(params).encode('utf-8')
+    req = Request('http://210.77.16.21/eportal/InterFace.do?method=login', data=data)
+    loginResp = urlopen(req)
+    loginResult = json.loads(loginResp.read().decode('utf-8'))
 
-        if loginResult['result'] == 'fail':
-            print(loginResult['message'])
-        elif loginResult['result'] == 'success':
-            print('Congratulations, network is connected now!')
-            return True
-        else:
-            print('Unknown error!') 
+    if loginResult['result'] == 'fail':
+        print(loginResult['message'])
+    elif loginResult['result'] == 'success':
+        print('Congratulations, network is connected now!')
+        return True
+    else:
+        print('Unknown error!') 
+
     return False
 
 
@@ -77,26 +78,33 @@ def status():
 def print_usage():
     print("")
     print("ucas.py is a python script for ucas network")
-    print("usage: python[3] ucas.py command [params]")
+    print("usage: python[2/3] ucas.py command [params]")
     print("    commands:")
     print("        login [-p] [username] [password]")
     print("            login to ucas network, e.g. python3 ucas.py login user123 pass123")
     print("            -p, --prefix: add '发\\' before username")
     print("        logout")
     print("            logout from ucas network")
-    print("        find")
+    print("        find [-s] [startRoom]")
     print("            find an account from ucas icc...")
+    print("            -s, --start: start from a specific room")
     print("        status")
     print("            status of network")
 
 
-def find(startFloor=1):
-    for floor in range(startFloor, 8):
-        for room in range(1, 15):
+def find(start=101):
+    startFloor = start // 100
+    startRoom = 1
+    isStart = True
+    for floor in range(startFloor, 9):
+        if isStart:
+            startRoom = start % 100
+            isStart = False
+        for room in range(startRoom, 20):
             for ab in {'a', 'b'}:
                 user = "2{}{:0>2}{}".format(floor, room, ab)
                 password = "2{}{:0>2}".format(floor, room)
-                print("user:" + user + " pass:" + password)
+                print("username:" + user + "\tpassword:" + password)
                 if login(user, password):
                     return True
     return False
@@ -111,15 +119,15 @@ if __name__ == '__main__':
             login(sys.argv[3], sys.argv[4], True)
         else:
             login(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'find':
+        start = 101
+        if len(sys.argv) > 3 and ( sys.argv[2] == '--start' or sys.argv[2] == '-s'):
+            start = int(sys.argv[3])
+        find(start)
     elif sys.argv[1] == 'logout':
         logout()
     elif sys.argv[1] == 'status':
         status()
-    elif sys.argv[1] == 'find':
-        skip = 0
-        if len(sys.argv) > 3 and ( sys.argv[2] == '--floor' or sys.argv[2] == '-f'):
-            skip = int(sys.argv[3])
-        find(skip)
     else:
         print("Parameter Error!")
         print_usage()
